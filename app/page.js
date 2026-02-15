f52484868eac47eac52b00138792de3d736d3445
nafisa4611 <jabinmila@gmail.com>
2026-02-16 01:17:05 +0600

    Main UI created

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import MainUI from "./Components/MainUi";
 
 
 
@@ -6,7 +7,7 @@ export default function page() {
   return (
     <>
       <div className=''>
-        <h1 className='text-primary text-3xl text-center'>Welcome to DashBoard UI</h1>
+        <MainUI />
       </div>
 
 
